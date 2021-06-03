e233c922a9dff9c9a91edf4df896a7e6aa7fbe93
FFrog <ljw1101.vip@gmail.com>
2021-06-03 11:48:52 +0800

    update vscode/keybindings.json & vscode/settings.json

---
+++ acm/bestSightseeingPair.cpp
@@ -0,0 +1,45 @@
+//**********************************************************************************************
+//
+//    Filename:         bestSightseeingPair.cpp
+//    Author:           lijiawei
+//    Create at:        2021/06/02 15:19:39
+//    Link:             https://leetcode-cn.com/problems/best-sightseeing-pair/
+//    Description:      Best Sightseeing Pair
+//
+//**********************************************************************************************
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    Solution()
+    {
+    }
+    int maxScoreSightseeingPair(vector<int> &values)
+    {
+        int n = values.size();
+        int max = values[0];
+        int rec = 0;
+
+        for (int j = 1; j < n; j++) {
+            max = (values[j - 1] + j - 1) > max ? values[j - 1] + j - 1 : max;
+            rec = max + values[j] - j > rec ? max + values[j] - j : rec;
+        }
+
+        return rec;
+    }
+};
+
+int main()
+{
+    vector<int> vec = {8, 1, 5, 2, 6};
+
+    Solution sol;
+    cout << sol.maxScoreSightseeingPair(vec) << endl;
+
+    return 0;
+}
