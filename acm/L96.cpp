f9d99ef6228c68eb0740d8640cfaa3b7fb3bcd14
FFrog <ljw1101.vip@gmail.com>
2021-11-11 12:49:37 +0800

    update acm/L96.cpp

--- acm/L96.cpp
+++ acm/L96.cpp
@@ -5,34 +5,41 @@ using namespace std;
 
 class Solution {
 public:
+    Solution(int n)
+    {
+        record.resize(n + 1);
+        record[0] = 1;
+    }
+
     int numTrees(int n)
     {
-        vector<int> ans(n + 1, 0);
-        ans[0] = 1;
-
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                ans[i] += ans[j - 1] * ans[i - j];
+                record[i] += record[j - 1] * record[i - j];
             }
         }
 
-        return ans[n];
+        return record[n];
     }
 
     int numTrees2(int n)
     {
         int ans = 0;
 
-        if (n == 0 || n == 1) {
-            ans = 1;
+        if (record[n]) {
+            return record[n];
         } else {
             for (int i = 1; i <= n; i++) {
                 ans += numTrees2(i - 1) * numTrees2(n - i);
             }
+            record[n] = ans;
         }
 
         return ans;
     }
+
+private:
+    vector<int> record;
 };
 
 int main()
@@ -41,9 +48,8 @@ int main()
 
     cin >> n;
 
-    Solution sol;
+    Solution sol(n);
     cout << sol.numTrees(n) << endl;
-
     cout << sol.numTrees2(n) << endl;
 
     return 0;
