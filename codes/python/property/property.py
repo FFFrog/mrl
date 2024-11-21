f39436f3cbfc8ca919878e0926946832de5cda1a
FFFrog <ljw1101.vip@gmail.com>
2024-11-21 18:50:27 +0800

    update property.py

--- codes/python/property/property.py
+++ codes/python/property/property.py
@@ -1,22 +1,30 @@
 import os
 
-class Descriptor:
 
+class Descriptor:
     def __get__(self, instance, owner):
         if instance is None:
-            print('__get__(): Accessing x from the class', instance,owner)
+            print('__get__(): Accessing x from the class', self, instance, owner)
             return self
         
-        print('__get__(): Accessing x from the object', instance,owner)
-        return 'X from descriptor'
+        print('__get__(): Accessing x from the object', self, instance, owner)
+        return self
 
     def __set__(self, instance, value):
         print('__set__(): Setting x on the object', instance)
 
+
 class Foo:
     x = Descriptor()
 
-Foo.x
 
-a = Foo()
-a.x
+f = Foo()
+
+print(Foo.x)
+print(type(Foo.x))
+
+print(Foo.x.__get__)
+print(type(Foo.x.__get__))
+
+print(f.x)
+print(type(f.x))
